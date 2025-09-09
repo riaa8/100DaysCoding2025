a0a426ee9ee7673fe0b759c86c1db5566884a00c
riaa8 <ria302225@gmail.com>
2025-09-09 20:31:43 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,13 @@
+package DayOfCoding;
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.print("Okelah");
+        System.out.println("Yayayaya");
+        
+        String camp = "Universitas Sulawesi Barat";
+        System.out.printf("Universitas : %s", camp);
+        
+
+    }
+    
+}
