c2bad1036a826a0ebd76d8a836993b5f73f2c930
riaa8 <ria302225@gmail.com>
2025-09-10 17:47:46 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,27 @@
+package DayOfCoding;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        
+        //println() --> untuk penggunaan baris baru
+        System.out.println("Saya");
+        
+        //print() --> tidak menggunakan baris baru
+        System.out.print("Nama ku :");
+        System.out.print("RIA RAMADANI \n");
+        
+        // printf() --> untuk format penggunaan tipe data
+        // %s : untuk String 
+        // %d : untuk int (decimal/bilangan bulat)
+        String camp = "Unsulbar";
+        int umur = 20;
+        System.out.printf("Kampusku : %s, dan Umurku %d\n", camp, umur);
+
+        System.out.println("Ini mi saja dulu na __><__ ");
+        
+
+      
+    }
+    
+}
