ca2a14ffe654a888bb6bab553feeec62cbc8cbab
riaa8 <ria302225@gmail.com>
2025-09-13 20:25:13 +0800

    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ Day7.java
@@ -0,0 +1,14 @@
+package DayOfCoding;
+public class Day7 {
+    public static void main(String[] args) {
+        char huruf = 'R'; //menyimpan satu karakter/huruf atau nilai ASCII
+        boolean benar = true; //menyimpan nilai benar
+        boolean salah = false; //menyimapn nilai salah
+        
+        System.out.println("Char: " + huruf);
+        System.out.println("Bolean Benar: " + benar);
+        System.out.println("Bollean Salah: " + salah);
+        
+    }
+    
+}
