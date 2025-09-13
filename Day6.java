9f85c09974791acc0367e91a1255294446fbf827
riaa8 <ria302225@gmail.com>
2025-09-13 20:09:43 +0800

    Tipe Data Pecahan (double, float).

---
+++ Day6.java
@@ -0,0 +1,14 @@
+package DayOfCoding;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        double angka1 = 3.123467842254; //double Menyimpan angka pecahan. Cukup untuk menyimpan 15 hingga 16 digit desimal.
+        float angka2 = 2.34567f; //float Menyimpan angka pecahan. Cukup untuk menyimpan 6 hingga 7 digit desimal.
+
+
+        System.out.println("Double : " + angka1); //untuk mencetak double
+        System.out.println("Float : " + angka2); //untuk mencetak float
+    }
+
+}
