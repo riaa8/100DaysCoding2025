6876b01286bf6347359e0fc18971b080404091e3
riaa8 <ria302225@gmail.com>
2025-09-15 22:33:18 +0800

    Pengenalan Tipe Data String untuk teks.

---
+++ Day8.java
@@ -0,0 +1,11 @@
+package DayOfCoding;
+public class Day8 {
+    public static void main(String[] args) {
+        String nama = "RIA RAMADANI";
+        String kampus = "Universitas Sulawesi Barat";
+        
+        System.out.println(nama);
+        System.out.println(kampus);
+    }
+    
+}
