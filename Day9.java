471651c8f3ba6bcfd1a71afe84020605888cd457
riaa8 <ria302225@gmail.com>
2025-09-16 22:44:10 +0800

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Day9.java
@@ -0,0 +1,20 @@
+package DayOfCoding;
+public class Day9 {
+    public static void main(String[] args) {
+        String N;
+        int U;
+        
+        N = "Ria Ramadani";
+        U = 20; // deklarasi + inisialisasi
+        System.out.println("Nama : " + N);
+        System.out.println("Umur: " + U);
+        
+        N = "Ria";
+        U = 21; // update nilai
+        System.out.println("Nama setelah diupdate: " + N);
+        System.out.println("Umur setelah di update : " + U);
+
+    }
+}
+
+
