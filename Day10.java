724f9fe19a59275c8a68fd61923486ca4cfa9b3f
riaa8 <ria302225@gmail.com>
2025-09-17 13:48:07 +0800

    Constanta (Final Variabel).

---
+++ Day10.java
@@ -0,0 +1,10 @@
+package DayOfCoding;
+public class Day10 {
+    public static void main(String[] args) {
+        final double IP = 3.58; // nilai tidak bisa diubah
+        System.out.println("Nilai IP: " + IP);
+
+        // IP = 3.15; // ERROR kalau dicoba
+    }
+}
+
