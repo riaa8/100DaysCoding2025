46ce65f11a074f93977bd53c42f563dccc1b48cb
riaa8 <ria302225@gmail.com>
2025-09-17 14:12:34 +0800

    Menerima Input dari Keyboard (pengenalan Scanner).

---
+++ Day11.java
@@ -0,0 +1,19 @@
+package DayOfCoding;
+
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in); 
+       
+        System.out.println("Masukkan nama:");
+        String nama = sc.nextLine();
+        
+        System.out.println("Masukkan umur :");
+        int umur = sc.nextInt();
+        
+        System.out.println("Hay nama saya " + nama + " Umur saya " + umur + " tahun");
+        
+    }
+    
+}
