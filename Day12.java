8409120f117f34133f4f8369a3dcc314c2e6830b
riaa8 <ria302225@gmail.com>
2025-09-19 21:39:46 +0800

    Latihan: Membuat Program Biodata Sederhana.

---
+++ Day12.java
@@ -0,0 +1,28 @@
+package DayOfCoding;
+
+import java.util.Scanner;
+
+public class Day12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Masukkan nama:");
+        String nm = sc.nextLine();
+        
+        System.out.print("masukkan umur:");
+        int um = sc.nextInt();
+        
+        System.out.print("masukkan alamat:");
+        String alamat = sc.nextLine();
+        
+        System.out.print("masukkan univ: ");
+        String un = sc.nextLine();
+        
+        System.out.println("==============BOIDATA=============");
+        System.out.println("Nama : " + nm);
+        System.out.println("Umur : " + um);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kampaus : " + un);
+    }
+    
+}
