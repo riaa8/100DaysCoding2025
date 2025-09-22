8cb41299de0cb59528c454363252112d6181a8f5
riaa8 <ria302225@gmail.com>
2025-09-22 20:10:25 +0800

    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ Day15.java
@@ -0,0 +1,11 @@
+package Package;
+public class Day15 {
+    public static void main(String[] args) {
+        int ang1 = 18;
+        int ang2 = 17;
+        
+        System.out.println("18 + 17 = " + (ang1 + ang2));
+        System.out.println("18 - 17 = " + (ang1 - ang2));
+    }
+    
+}
