63fa1bc7eeb8ab9c8e67840041fe560e0bc13769
riaa8 <ria302225@gmail.com>
2025-09-23 21:55:42 +0800

    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ Day16.java
@@ -0,0 +1,14 @@
+package Package;
+public class Day16 {
+    public static void main(String[] args) {
+        int ang1 = 5;
+        int ang2 = 25;
+        
+        System.out.println("===OPERATOR ARITMATIKA PERKALIAN DAN PEMBAGIAN===");
+        System.out.println("5 x 25 = " +(ang1 * ang2));
+        System.out.println("5 x 5 = " +(ang1 * ang1));
+        System.out.println("25 / 5 = " +(ang2 / ang1));
+        System.out.println("25 / 25 = " +(ang2 / ang2));
+    }
+    
+}
