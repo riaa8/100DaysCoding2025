7c0afb744e12509e42cc1a040f6a4886aecf9c69
riaa8 <ria302225@gmail.com>
2025-09-24 23:30:33 +0800

    Operator Aritmatika Modulus atau Sisa Bagi (%)

---
+++ Day17.java
@@ -0,0 +1,13 @@
+package Package;
+public class Day17 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = 6;
+        
+        System.out.println("===OPERATOR ARITMATIKA MODULUS ATAU SISA BAGI===");
+        System.out.println("6 % 4 = " + (c % a));
+        System.out.println("5 % 4 = " + (b % a));
+        System.out.println("6 % 5 = " + (c % b));
+    }    
+}
