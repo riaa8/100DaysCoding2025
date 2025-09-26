8edff68f87e5504c35d8faea76c9b7b7288a4591
riaa8 <ria302225@gmail.com>
2025-09-26 22:02:40 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Day19.java
@@ -0,0 +1,14 @@
+package Package;
+public class Day19 {
+    public static void main(String[] args) {
+        double angka = 9.99;
+        int hasil = (int) angka; // casting paksa
+        System.out.println("Hasil konversi manual: " + hasil);
+        
+        float b = 10.11f;
+        byte jadi = (byte) b;
+        System.out.println("hasilnya: " + jadi);
+    }
+}
+
+
