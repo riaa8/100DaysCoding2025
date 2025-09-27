39bc280c277a919a08c8653aef16f9f4a1d31e15
riaa8 <ria302225@gmail.com>
2025-09-27 21:06:20 +0800

    Mengubah Tipe Data Primitif menjadi String.

---
+++ Day20.java
@@ -0,0 +1,11 @@
+package Package;
+public class Day20 {
+    public static void main(String[] args) { 
+        int angka = 20;
+        String teks = String.valueOf(angka);
+
+        System.out.println("Int asli bisa dihitung: " + (angka + 10));
+        System.out.println("String tidak bisa dihitung, hanya digabung teks: " + (teks + 10));
+    }
+    
+}
