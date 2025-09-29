4f69f28585fcf02287d29dae650a1aec8b9d6e2e
riaa8 <ria302225@gmail.com>
2025-09-29 23:45:54 +0800

    Latihan: Program Luas Persegi

---
+++ Day22.java
@@ -0,0 +1,10 @@
+package Package;
+public class Day22 {
+    public static void main(String[] args) {
+        //mengi=hitung luas persegi
+        int sisi = 10;
+        int luas = sisi * sisi;
+        System.out.println("Luas persegi = " + luas);
+    }
+    
+}
