0fdc74e338288d9ad2d6c3ecca559558ae1da687
riaa8 <ria302225@gmail.com>
2025-09-30 22:06:07 +0800

    Latihan: luas persegi panjang

---
+++ Day23.java
@@ -0,0 +1,12 @@
+package Package;
+public class Day23 {
+    public static void main(String[] args) {
+        //Latihan: luas persegi panjang
+        int panjang = 5;
+        int lebar = 4;
+        
+        int luas = panjang * lebar;
+        System.out.println("luas persegi panjang = "+ luas);
+    }
+    
+}
