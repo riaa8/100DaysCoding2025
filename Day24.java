3af87f34ec98dfbb0d13eb0325938fdb2f376e71
riaa8 <ria302225@gmail.com>
2025-10-01 23:15:52 +0800

    Latihan: luas lingkaran

---
+++ Day24.java
@@ -0,0 +1,12 @@
+package Package;
+public class Day24 {
+    public static void main(String[] args) {
+        //Latihan: luas Lingkaran
+        int jari = 5;
+        final double PI = 3.14159;
+        
+        double luas = PI * jari * jari;
+        System.out.println("hasil dari luas lingkaran = " + luas);
+    }
+    
+}
