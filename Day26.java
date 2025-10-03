90cf0e28f8b32bfdbda4dd7003684fe29df8e36c
riaa8 <ria302225@gmail.com>
2025-10-03 23:11:32 +0800

    Operator penugasan (=, +=, -=, dll)

---
+++ Day26.java
@@ -0,0 +1,20 @@
+package Package;
+public class Day26 {
+    public static void main(String[] args) {
+        //Operator Penugasan (=, +=, -=, dll).
+        int x = 10;
+         
+        x += 5; // sama dengan x = x + 5
+        System.out.println("x += 5: " + x);
+
+        x -= 3; // sama dengan x = x - 3
+        System.out.println("x -= 3: " + x);
+
+        x *= 2; // sama dengan x = x * 2
+        System.out.println("x *= 2: " + x);
+
+        x /= 4; // sama dengan x = x / 4
+        System.out.println("x /= 4: " + x);
+    }
+    
+}
