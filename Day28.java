f2d136c97e55a999ade91d32625ad3416ec0d1de
riaa8 <ria302225@gmail.com>
2025-10-05 20:31:34 +0800

    Operator Perbandingan == dan !=

---
+++ Day28.java
@@ -0,0 +1,16 @@
+package Package;
+public class Day28 {
+    public static void main(String[] args) {
+        //Operator Perbandingan == dan !=
+        
+        int a = 5;
+        int b = 5;
+        int c = 8;
+        
+        System.out.println(a == b);
+        System.out.println(a != c);
+        System.out.println(a == c);
+        System.out.println(a != b);
+    }
+    
+}
