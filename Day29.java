d1fa67e187d7e2d0e743a13b3de89b10f549a779
riaa8 <ria302225@gmail.com>
2025-10-06 22:03:36 +0800

    Operator Perbandingan < dan >

---
+++ Day29.java
@@ -0,0 +1,12 @@
+package Package;
+public class Day29 {
+    public static void main(String[] args) {
+        //Operator Perbandingan < dan >
+        int x = 3;
+        int y = 6;
+        
+        System.out.println(x < y);
+        System.out.println(x > y);
+    }
+    
+}
