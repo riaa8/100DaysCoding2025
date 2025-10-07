16acde59cffbbc160935c64d51bb18ff8a660c9a
riaa8 <ria302225@gmail.com>
2025-10-07 22:31:22 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,16 @@
+package Package;
+public class Day30 {
+    public static void main(String[] args) {
+        //Operator Perbandingan <= dan >=
+        int a = 5;
+        int b = 5;
+        int c = 10;
+        
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(b >= c);
+        System.out.println(b <= c);
+        
+    }
+    
+}
