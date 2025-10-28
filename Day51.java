cf7be13502f82f7240c6172d18047670af59a973
riaa8 <ria302225@gmail.com>
2025-10-28 19:19:58 +0800

    Perulangan for

---
+++ Day51.java
@@ -0,0 +1,9 @@
+package Package;
+public class Day51 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Perulangan ke" + i);
+        }
+    }
+    
+}
