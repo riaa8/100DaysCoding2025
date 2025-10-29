fe4eb8af1bcce14bbd8c26ec5ad0660d46d17555
riaa8 <ria302225@gmail.com>
2025-10-29 22:12:16 +0800

    Perulangan while

---
+++ Day52.java
@@ -0,0 +1,12 @@
+package Package;
+public class Day52 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 5) {
+            System.out.println("Hitungan ke-" + i);
+            i++;
+        }
+    }
+}
+
+    
