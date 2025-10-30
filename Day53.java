c5c22b541f82a2264d5035bec17f5caad7d750bc
riaa8 <ria302225@gmail.com>
2025-10-30 21:41:54 +0800

    Keyword break

---
+++ Day53.java
@@ -0,0 +1,10 @@
+package Package;
+public class Day53 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i == 8) break; // berhenti di angka 8
+            System.out.println("i = " + i);
+        }
+        System.out.println("Loop berhenti di angka 8");
+    }
+}
