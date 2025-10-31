b24575b5d1487c359ac9c885a41839a75c091d8e
riaa8 <ria302225@gmail.com>
2025-10-31 23:30:11 +0800

    Keyword continue

---
+++ Day54.java
@@ -0,0 +1,11 @@
+package Package;
+public class Day54 {
+    public static void main(String[] args) {
+         for (int i = 1; i <= 10; i++) {
+            if (i == 8) continue; // lewati angka 8
+            System.out.println("i = " + i);
+        }
+        System.out.println("Angka 3 dilewati");
+    }
+    
+}
