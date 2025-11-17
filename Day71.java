d7a75939471d22cdfca837c8d60e593f9ee25f0e
riaa8 <ria302225@gmail.com>
2025-11-17 20:50:35 +0800

    Latihan: Membuat Pola Segi Tiga Siku-siku

---
+++ Day71.java
@@ -0,0 +1,20 @@
+package Package;
+
+import java.util.Scanner;
+
+public class Day71 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan tinggi segitiga: ");
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+                
+            }
+            System.out.println();
+        }
+    }
+    
+}
