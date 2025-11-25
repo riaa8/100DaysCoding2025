ef66b308790f7a085ac2f737f7be873635df4d00
riaa8 <ria302225@gmail.com>
2025-11-25 21:37:41 +0800

    Array 1D

---
+++ Day80.java
@@ -0,0 +1,12 @@
+package Package;
+
+import java.util.Scanner;
+
+public class Day80 {
+    public static void main(String[] args) {
+       int[] angka = {10, 20, 30, 40, 50};
+       
+        System.out.println("Elemen ke-3: " + angka[2]);
+    }
+    
+}
