29ecc8e003e56f531b731f65f21e382f528b9b76
riaa8 <ria302225@gmail.com>
2025-11-27 21:16:58 +0800

    Latihan: Akses Elemen Array Dengan Indeks

---
+++ Day81.java
@@ -0,0 +1,11 @@
+package Package;
+public class Day81 {
+    public static void main(String[] args) {
+        int[] nilai = {5, 10, 15, 20};
+
+        System.out.println("Indeks 0 = " + nilai[0]);
+        System.out.println("Indeks 1 = " + nilai[1]);
+        System.out.println("Indeks 2 = " + nilai[2]);
+        System.out.println("Indeks 3 = " + nilai[3]);
+    }
+}
