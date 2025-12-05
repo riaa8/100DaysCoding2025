5e6e661acaafeb303d99fa3a9bcd3bbdcdcfd4a1
riaa8 <ria302225@gmail.com>
2025-12-05 21:23:08 +0800

    Method void

---
+++ Day89.java
@@ -0,0 +1,10 @@
+package Package;
+public class Day89 {
+    //Method void
+    static void sayHello(String name){
+        System.out.println("Hay, " + name + "!");
+    }
+    public static void main(String[] args) {
+        sayHello("Riaaaa");
+    }
+}
