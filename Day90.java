a6b1e3972e8d8fa7cbce1e749c879223ffa12c24
riaa8 <ria302225@gmail.com>
2025-12-06 20:37:20 +0800

    Method non-void

---
+++ Day90.java
@@ -0,0 +1,12 @@
+package Package;
+public class Day90 {
+    //Method non-void
+    static int tambah(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int hasil = tambah(2,3);
+        System.out.println("hasil = " + hasil);
+    }
+    
+}
