beb0e0e41dfa0c4cb8f7a3123db9d36d7d635e7c
riaa8 <ria302225@gmail.com>
2025-12-07 21:04:37 +0800

    Keyword return

---
+++ Day92.java
@@ -0,0 +1,14 @@
+package Package;
+public class Day92 {
+
+    static int kuadrat(int x) {
+        return x * x; // mengembalikan nilai ke pemanggil
+    }
+
+    public static void main(String[] args) {
+        int r = kuadrat(5);
+        System.out.println("5^2 = " + r);
+    }
+}
+    
+
