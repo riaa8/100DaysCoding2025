d98eb195b74d7f9635a216229a45f6c2e730301a
riaa8 <ria302225@gmail.com>
2025-12-09 18:50:10 +0800

    Method ceil(), floor() dan round()

---
+++ Day93.java
@@ -0,0 +1,13 @@
+package Package;
+public class Day93 {
+    public static void main(String[] args) {
+        //Method ceil(), floor() dan round()
+
+        double r = 3.53;
+        
+        System.out.println("ceil 3.53 = " + Math.ceil(r));
+        System.out.println("floor 3.53 = " + Math.floor(r));
+        System.out.println("round 3.53 = " + Math.round(r));
+    }
+    
+}
