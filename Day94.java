a1dbf8788d22101d352b4e9c36a3ba75db41a12c
riaa8 <ria302225@gmail.com>
2025-12-10 21:01:04 +0800

    Latihan: mencetak dengan method void

---
+++ Day94.java
@@ -0,0 +1,10 @@
+package Package;
+public class Day94 {
+    //Latihan: mencetak dengan method void
+    static void saya(){
+        System.out.println("Haloo, nama saya Riaaa");
+}    
+    public static void main(String[] args) {
+        saya();
+    }
+}
