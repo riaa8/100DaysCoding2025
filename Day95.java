de8fa6911906b56ab4a1af1546a52a1715b0842e
riaa8 <ria302225@gmail.com>
2025-12-11 19:58:56 +0800

    Latihan: Mencetak dengan Parameter Void

---
+++ Day95.java
@@ -0,0 +1,10 @@
+package Package;
+public class Day95 {
+//Latihan: Mencetak dengan Parameter Void
+    static void cetak(String pesan){
+        System.out.println("Pesan: " + pesan);
+    }
+    public static void main(String[] args) {
+        cetak("selamat pagi");
+    }
+}
