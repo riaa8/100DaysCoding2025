de4d6b5e9a4b3ce88e8a098897b3b402b9bc9957
riaa8 <ria302225@gmail.com>
2025-12-13 21:44:07 +0800

    Latihan: Menghitung Luas Persegi dengan Method Non-void dan parameternya

--- Day97.java
+++ Day97.java
@@ -3,7 +3,10 @@ package Package;
 import java.util.Scanner;
 
 public class Day97 {
-    static int persegi(int sisi){return sisi * sisi;}
+    //Latihan: Menghitung Luas Persegi dengan Method Non-void dan parameternya
+    static int persegi(int sisi){
+        return sisi * sisi;
+    }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
